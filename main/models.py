18c84c4147e59dba09d914f1a13a39178ea1fd4e
Jorge Otoniel <otto.7day@hotmail.com>
2016-11-03 18:15:03 -0600

    Después de crear la BD

--- main/models.py
+++ main/models.py
@@ -2,4 +2,68 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class Puesto(models.Model):
+    id = models.IntegerField(editable=False, auto_created=True, primary_key=True)
+    nombre = models.TextField(max_length=30, null=False)
+    salario = models.DecimalField(max_digits=5, decimal_places=5, null=False)
+
+
 # Create your models here.
+class Empleado(models.Model):
+    sexo_opt = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    )
+
+    id = models.IntegerField(editable=False, auto_created=True, primary_key=True)
+    nombres = models.TextField(max_length=50, null=False)
+    apellidos = models.TextField(max_length=50, null=False)
+    edad = models.IntegerField(null=False)
+    sexo = models.CharField(max_length=1, choices=sexo_opt, null=False)
+    direccion = models.TextField(max_length=200, null=False)
+    telefono = models.CharField(max_length=9, null=False)
+    contacto = models.CharField(max_length=9, null=False)
+    dui = models.CharField(max_length=10, null=False)
+    nit = models.CharField(max_length=17, null=False)
+    afp = models.CharField(max_length=12, null=False)
+    puesto = models.ForeignKey(Puesto, on_delete=models.CASCADE)
+    activo = models.BooleanField()
+
+
+class Usuario(models.Model):
+    id = models.TextField(max_length=25, primary_key=True, auto_created=False, editable=True)
+    password = models.CharField(max_length=64, null=False)
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+
+
+class Proveedor(models.Model):
+    id = models.IntegerField(editable=False, auto_created=True, primary_key=True)
+    marca = models.CharField(max_length=50, null=False)
+    direccion = models.TextField(max_length=200, null=False)
+    telefono = models.CharField(max_length=9, null=False)
+    nit = models.CharField(max_length=17, null=False)
+
+
+class TipoTransaccion(models.Model):
+    id = models.IntegerField(editable=False, auto_created=True, primary_key=True)
+    nombre = models.CharField(max_length=50, null=False)
+
+
+class TipoCuenta(models.Model):
+    id = models.IntegerField(editable=False, auto_created=True, primary_key=True)
+    nombre = models.CharField(max_length=50, null=False)
+
+
+class Cuenta(models.Model):
+    id = models.IntegerField(editable=False, auto_created=True, primary_key=True)
+    saldoInicial = models.DecimalField(max_digits=5, decimal_places=5)
+    debe = models.DecimalField(max_digits=5, decimal_places=5)
+    haber = models.DecimalField(max_digits=5, decimal_places=5)
+    saldoFinal = models.DecimalField(max_digits=5, decimal_places=5)
+
+
+class Cliente(models.Model):
+    id = models.IntegerField(editable=False, auto_created=True, primary_key=True)
+    nombre = models.CharField(max_length=100, null=False)
+    telefono = models.CharField(max_length=9, null=False)
