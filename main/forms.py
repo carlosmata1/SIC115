319632f22a725585abafe428997e064891891443
Jorge Otoniel <otto.7day@hotmail.com>
2016-11-20 14:17:40 -0600

    * Cambios mínimos * Se corrigieron algunas advertencias

--- main/forms.py
+++ main/forms.py
@@ -20,45 +20,40 @@ class LoginForm(forms.Form):
 
 
 class CuentaForm(forms.Form):
-    nombre=forms.CharField(required=True,
-                           label='Nombre de la cuenta')
-    rubro=forms.ModelChoiceField(required=True,
-                                 queryset=Rubro.objects.all())
-    tipo=forms.ChoiceField(required=True,
-                           choices=tiposCuentas)
+    nombre = forms.CharField(required=True,
+                             label='Nombre de la cuenta')
+    rubro = forms.ModelChoiceField(required=True,
+                                   queryset=Rubro.objects.all())
+    tipo = forms.ChoiceField(required=True,
+                             choices=tiposCuentas)
 
 
 class MovimientoForm(forms.Form):
-    cuenta=forms.ModelChoiceField(label='Eliga la cuenta a realizar el movimiento',
-                                  queryset=Cuenta.objects.all())
-
-
-    tipo=forms.MultipleChoiceField(
+    cuenta = forms.ModelChoiceField(label='Eliga la cuenta a realizar el movimiento',
+                                    queryset=Cuenta.objects.all())
+    tipo = forms.MultipleChoiceField(
         required=False,
-        widget=forms.CheckboxSelectMultiple(attrs={'id':'test6p','checked':'checked'}),
+        widget=forms.CheckboxSelectMultiple(attrs={'id': 'test6p', 'checked': 'checked'}),
         choices=debes,
     )
-    cantidad=forms.DecimalField(label='Cantidad a transferir a Cuenta:',max_digits=10,decimal_places=2,min_value=0)
-
+    cantidad = forms.DecimalField(label='Cantidad a transferir a Cuenta:', max_digits=10, decimal_places=2, min_value=0)
 
 
 class TransaccionForm(forms.Form):
-    monto=forms.DecimalField(label='monto de transaccion',max_digits=10, decimal_places=2,min_value=0.0)
-    empleado=forms.ModelChoiceField(required=True,
-                                    queryset=Empleado.objects.all())
-    tipo=forms.ModelChoiceField(required=True,
-                                queryset=TipoTransaccion.objects.all())
-    descripcion=forms.CharField(widget=forms.Textarea)
-    fecha=forms.DateField()
-
+    monto = forms.DecimalField(label='monto de transaccion', max_digits=10, decimal_places=2, min_value=0.0)
+    empleado = forms.ModelChoiceField(required=True,
+                                      queryset=Empleado.objects.all())
+    tipo = forms.ModelChoiceField(required=True,
+                                  queryset=TipoTransaccion.objects.all())
+    descripcion = forms.CharField(widget=forms.Textarea)
+    fecha = forms.DateField()
 
 
 class EmpleadoForm(forms.ModelForm):
-
     class Meta:
-        model=Empleado
+        model = Empleado
 
-        fields=[
+        fields = [
             'nombres',
             'apellidos',
             'edad',
@@ -72,33 +67,33 @@ class EmpleadoForm(forms.ModelForm):
             'puesto',
             'activo',
         ]
-        labels={
+        labels = {
             'nombres': 'Nombres',
-            'apellidos':'Apellidos',
+            'apellidos': 'Apellidos',
             'edad': 'Edad',
             'sexo': 'Sexo',
-            'direccion':'Direccion',
-            'telefono':'Teléfono',
-            'contacto':'Contacto',
-            'dui':'DUI',
-            'nit':'NIT',
-            'afp':'AFP',
-            'puesto':'Puesto',
-            'activo':'Activo',
+            'direccion': 'Direccion',
+            'telefono': 'Teléfono',
+            'contacto': 'Contacto',
+            'dui': 'DUI',
+            'nit': 'NIT',
+            'afp': 'AFP',
+            'puesto': 'Puesto',
+            'activo': 'Activo',
         }
-        widgets={
-            'nombres': forms.TextInput (attrs={'class':'input-field col s3'}),
-            'apellidos':forms.TextInput (attrs={'class':'input-field '}),
-            'edad':forms.NumberInput(attrs={'class':'input-field '}),
-            'sexo':forms.Select(attrs={'class':'input-field '}),
-            'direccion':forms.TextInput (attrs={'class':'input-field'}),
-            'telefono':forms.NumberInput (attrs={'class':'input-field '}),
-            'contacto':forms.TextInput (attrs={'class':'input-field'}),
-            'dui':forms.TextInput (attrs={'class':'input-field '}),
-            'nit':forms.TextInput (attrs={'class':'input-field '}),
-            'afp':forms.TextInput (attrs={'class':'input-field'}),
-            'puesto':forms.Select(attrs={'class':'input-field'}),
-            'activo':forms.CheckboxSelectMultiple(attrs={'type':'checkbox'}),
+        widgets = {
+            'nombres': forms.TextInput(attrs={'class': 'input-field col s3'}),
+            'apellidos': forms.TextInput(attrs={'class': 'input-field '}),
+            'edad': forms.NumberInput(attrs={'class': 'input-field '}),
+            'sexo': forms.Select(attrs={'class': 'input-field '}),
+            'direccion': forms.TextInput(attrs={'class': 'input-field'}),
+            'telefono': forms.NumberInput(attrs={'class': 'input-field '}),
+            'contacto': forms.TextInput(attrs={'class': 'input-field'}),
+            'dui': forms.TextInput(attrs={'class': 'input-field '}),
+            'nit': forms.TextInput(attrs={'class': 'input-field '}),
+            'afp': forms.TextInput(attrs={'class': 'input-field'}),
+            'puesto': forms.Select(attrs={'class': 'input-field'}),
+            'activo': forms.CheckboxSelectMultiple(attrs={'type': 'checkbox'}),
         }
 
     def clean_nombres(self):
